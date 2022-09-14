6f98985b38289b205fe91703cbfe564138ba8a0e
centinela1024tb <centinela1024tb@gmail.com>
2022-09-14 13:58:23 -0500

    [ADD] Se implementa el servicio para veterinario
    => Servicio para guardar en la DB un registro
    => Servicio para verificar si ya esta registardo un email
    => Se renombro model Veterinario a Veterinaro
    => incluye validaciones en el controller registro

--- src/controllers/veterinarioController.ts
+++ src/controllers/veterinarioController.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from 'express';
+import { getVeterinario, saveRegistroVeterinario } from '../services/veterinarioServices';
 import { handleHttp } from '../utils/error.handle';
 
 
-const registrarVeterinario = ({ body }: Request, res: Response) => {
+const registrarVeterinario = async({ body }: Request, res: Response) => {
   try {
-    console.log(body)
+    const { name, email, password} = body;
+    // verificamos que email | password | name contengan datos
+    if(name && email && password) {    
+      // Verifificamos si existe el email  
+      const existEmail = await getVeterinario(body.email);  
+  
+      if (existEmail) {
+        // Si ya existe el emial mandamos mensaje de error
+        res.json({msg: 'Email already existed'});
+        console.log(body.email + ': Existe')
+      } else {
+        // Si no existe el email guaramos el registro
+        const newRegister = await saveRegistroVeterinario( body );
+        res.json(newRegister);
+      }
+    } else {
+      res.json({msg: 'Name, email and password is required'});
+    }    
   } catch (e) {    
-    handleHttp(res, 'ERROR_POST_REGISTER');
+    // handleHttp(res, 'ERROR_POST_REGISTER');
+    res.send({e})
   }
 }
 
